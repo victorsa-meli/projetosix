bdd53b69dd5baf869fcad4ffbcacd1706b20d852
victorsa <victoredo@gmail.com>
2024-03-17 19:37:15 -0300

    comit inicial

---
+++ src/main/java/com/projetosix/projetosix/ProjetosixApplication.java
@@ -0,0 +1,13 @@
+package com.projetosix.projetosix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetosixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projetosix/projetosix/ProjetosixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosix.projetosix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
