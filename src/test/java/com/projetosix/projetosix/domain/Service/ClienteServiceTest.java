3ae1e331dc419ce2cb0fd9cde6e4e4d7a43d3f8c
victorsa <victoredo@gmail.com>
2024-03-17 21:33:10 -0300

    inicializando testes

---
+++ src/test/java/com/projetosix/projetosix/domain/Service/ClienteServiceTest.java
@@ -0,0 +1,19 @@
+package com.projetosix.projetosix.domain.Service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ClienteServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void criarCliente() {
+    }
+
+    @Test
+    void consultarTodos() {
+    }
+}
